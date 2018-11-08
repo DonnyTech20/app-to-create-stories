94344af3399346f86d9696b6be11f11a518ebb13
Donny Techiera <DonnyTech20@gmail.com>
2018-11-08 13:14:38 -0500

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://dontech:dondon30@ds155203.mlab.com:55203/storybookdevelopment',
+    googleClientID:'550133478273-13ifftsdiuiivrua2lhi9gfkp2eocn2r.apps.googleusercontent.com',
+    googleClientSecret:'0mhNRKSJ-6Mltljtjc-hqj8L'
+}
