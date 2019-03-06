6483d55f190fe0464a9f8ffa7d53ac0302bb0b5e
Donny Techiera <DonnyTech20@gmail.com>
2019-03-06 14:32:59 -0500

    updated UI

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
 
 // Passport Config
-require('./config/passport')(passport);
-
+require("./config/passport")(passport);
 
 // Load Routes
-const auth = require('./routes/auth');
+const auth = require("./routes/auth");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('It works');
+app.get("/", (req, res) => {
+  res.send("Welcome Back Dev Donny");
 });
 
 // Use Routes
-app.use('/auth', auth);
+app.use("/auth", auth);
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+  console.log(`Server started on port ${port}`);
 });
