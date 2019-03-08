0ab4ea6cffbc5b83d972f179c65ee637eda59f20
Donny Techiera <DonnyTech20@gmail.com>
2019-03-08 14:17:17 -0500

    added cookie-parser & express-session

--- app.js
+++ app.js
@@ -2,11 +2,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load User Model
+require("./models/User");
+
+// Load Keys
+const keys = require("./config/keys");
+
+
 // Passport Config
 require("./config/passport")(passport);
 
+
 // Load Routes
 const auth = require("./routes/auth");
+
+
+// Map Global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+  useMongoClient:true
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 const app = express();
 
@@ -14,8 +32,12 @@ app.get("/", (req, res) => {
   res.send("Welcome Back Dev Donny");
 });
 
-// Use Routes
+// Use Route
 app.use("/auth", auth);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 const port = process.env.PORT || 4000;
 
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
     mongoURI:'mongodb://dontech:dondon30@ds155203.mlab.com:55203/storybookdevelopment',
+
     googleClientID:'550133478273-13ifftsdiuiivrua2lhi9gfkp2eocn2r.apps.googleusercontent.com',
+
     googleClientSecret:'0mhNRKSJ-6Mltljtjc-hqj8L'
-}
+};
